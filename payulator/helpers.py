96fc7b3e37d70177ab221dc668d477ae030418a1
duarte-cleiton <duarte.cleiton@outlook.com>
2020-02-25 17:07:15 -0300

    feature: compute_period_interest_rate, build_principal_fn, amortize, summarize_amortized_loan

--- payulator/helpers.py
+++ payulator/helpers.py
@@ -1,6 +1,7 @@
+from datetime import date
 import math
 from copy import copy
-from typing import List, Union, Dict
+from typing import List, Union, Dict, Optional
 
 import numpy as np
 import pandas as pd
@@ -60,4 +61,161 @@ def to_date_offset(num_per_year: int) -> Union[pd.DateOffset, None]:
         d = pd.DateOffset(days=1)
     else:
         d = None
+    return d
+
+def compute_period_interest_rate(
+    interest_rate: float, 
+    compounding_freq: str, 
+    payment_freq: str
+) -> float:
+    """
+    Compute the interest rate per payment period given
+    an annual interest rate, a compounding frequency, and a payment
+    freq.
+    See the function :func:`freq_to_num` for acceptable frequencies.
+    Arguments:
+        interest_rate {float} -- Return rate for a given operation
+        compounding_freq {str} -- Interest compounding frequency period
+        payment_freq {str} -- Payment frequence period
+    
+    Returns:
+        float -- The period interest rate
+    """
+    i = interest_rate
+    j = freq_to_num(compounding_freq, allow_cts=True)
+    k = freq_to_num(payment_freq)
+
+    if np.isinf(j):
+        return math.exp(i / k) - 1
+    else:
+        return (1 + i / j) ** (j / k) - 1
+
+def build_principal_fn(
+    principal: float,
+    interest_rate: float,
+    compounding_freq: str,
+    payment_freq: str,
+    num_payments: int
+):
+
+    """
+    Compute the remaining loan principal, the loan balance,
+    as a function of the number of payments made.
+    Return the resulting function.
+    """
+    P = principal
+    I = compute_period_interest_rate(interest_rate, compounding_freq, payment_freq)
+    n = num_payments
+
+    def p(t):
+        if I == 0:
+            return P - t * P / n
+        else:
+            return P * (1 - ((1 + I) ** t - 1) / ((1 + I) ** n - 1))
+    return p
+
+def amortize(
+    principal: float,
+    interest_rate: float,
+    compounding_freq: str,
+    payment_freq: str,
+    num_payments: int
+) -> float:
+
+    """
+        Given the loan parameters
+
+    - ``principal``: (float) amount of loan, that is, the principal
+    - ``interest_rate``: (float) nominal annual interest rate
+      (not as a percentage), e.g. 0.1 for 10%
+    - ``compounding_freq``: (string) compounding frequency;
+      one of the keys of :const:`NUM_BY_FREQ`, e.g. "monthly"
+    - ``payment_freq``: (string) payments frequency;
+      one of the keys of :const:`NUM_BY_FREQ`, e.g. "monthly"
+    - ``num_payments``: (integer) number of payments in the loan
+      term
+
+    return the periodic payment amount due to
+    amortize the loan into equal payments occurring at the frequency
+    ``payment_freq``.
+    See the function :func:`freq_to_num` for valid frequncies.
+
+    Notes:
+
+    - https://en.wikipedia.org/wiki/Amortization_calculator
+    - https://www.vertex42.com/ExcelArticles/amortization-calculation.html
+    """
+
+    P = principal
+    I = compute_period_interest_rate(interest_rate, compounding_freq, payment_freq)
+    n = num_payments
+    if I == 0:
+        A = P / n
+    else:
+        A = P * I / (1 - (1 + I) ** (-n))
+
+    return A
+
+def summarize_amortized_loan(
+    principal: float,
+    interest_rate: float,
+    compounding_freq: str,
+    payment_freq: str,
+    num_payments: int,
+    fee: float = 0,
+    first_payment_date: Optional[str] = None,
+    decimals: int = 2
+) -> Dict:
+
+    A = amortize(principal, interest_rate, compounding_freq, payment_freq, num_payments)
+    p = build_principal_fn(
+        principal, interest_rate, compounding_freq, payment_freq, num_payments
+    )
+
+    n = num_payments
+    f = (
+        pd.DataFrame({"payment_sequence": range(1, n + 1)})
+        .assign(beginning_balance=lambda x: (x.payment_sequence - 1).map(p))
+        .assign(
+            principal_payment=lambda x: x.beginning_balance.diff(-1).fillna(
+                x.beginning_balance.iat[-1]
+            )
+        )
+        .assign(ending_balance=lambda x: x.beginning_balance - x.principal_payment)
+        .assign(interest_payment=lambda x: A - x.principal_payment)
+        .assign(fee_payment=0)
+        .assign(notes=np.nan)
+    )
+    f.fee_payment.iat[0] = fee
+
+    date_offset = to_date_offset(freq_to_num(payment_freq))
+    if first_payment_date and date_offset:
+        f["payment_date"] = [
+            pd.Timestamp(first_payment_date) + j * date_offset for j in range(n)
+        ]
+
+        # Put payment date first
+        cols = f.columns.tolist()
+        cols.remove("payment_date")
+        cols.insert(1, "payment_date")
+        f = f[cols].copy()
+
+    # Bundle the result into dictionary
+    d = {}
+    d["payment_schedule"] = f
+    d["periodic_payment"] = A
+    d["interest_total"] = f["interest_payment"].sum()
+    d["interest_and_fee_total"] = d["interest_total"] + fee
+    d["payment_total"] = d["interest_and_fee_total"] + principal
+    d["interest_and_fee_total/principal"] = d["interest_and_fee_total"] / principal
+    if "payment_date" in f:
+        d["first_payment_date"] = f.payment_date.iat[0].strftime("%Y-%m-%d")
+        d["last_payment_date"] = f.payment_date.iat[-1].strftime("%Y-%m-%d")
+    
+    if decimals is not None:
+        for key, val in d.items():
+            if isinstance(val, pd.DataFrame):
+                d[key] = val.round(decimals)
+            elif isinstance(val, float):
+                d[key] = round(val, 2)
     return d
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -39,3 +39,75 @@ def test_to_date_offset():
         assert isinstance(to_date_offset(k), pd.DateOffset)
 
     assert to_date_offset(10) is None
+
+def test_amortize():
+    A = amortize(1000, 0.05, "quarterly", "monthly", 3 * 12)
+    assert round(A, 2) == 29.96
+
+    A = amortize(1000, 0.02, "continuously", "semiannually", 2 * 2)
+    assert round(A, 2) == 256.31
+
+def test_compute_period_interest_rate():
+    I = compute_period_interest_rate(0.12, "monthly", "monthly")
+    assert round(I, 2) == 0.01
+
+def test_build_principal_fn():
+    balances = [
+        100.00,
+        91.93,
+        83.81,
+        75.65,
+        67.44,
+        59.18,
+        50.87,
+        42.52,
+        34.11,
+        25.66,
+        17.16,
+        8.60,
+        0,
+    ]
+
+    p = build_principal_fn(100, 0.07, "monthly", "monthly", 12)
+    for i in range(13):
+        assert round(p(i), 2) == balances[i]
+
+def test_summarize_amortized_loan():
+    s = summarize_amortized_loan(
+        1000,
+        0.05,
+        "quarterly",
+        "monthly",
+        3 * 12,
+        fee=10,
+        first_payment_date="2018-01-01"
+    )
+    expect_keys = {
+        "payment_schedule",
+        "periodic_payment",
+        "interest_total",
+        "interest_and_fee_total",
+        "payment_total",
+        "interest_and_fee_total/principal",
+        "first_payment_date",
+        "last_payment_date"
+    }
+    assert set(s.keys()) == expect_keys
+    assert round(s["periodic_payment"], 2) == 29.96
+    assert round(s["interest_and_fee_total"], 2) == 88.62
+
+    #Check payment schedule
+    f = s["payment_schedule"]
+    assert set(f.columns) == {
+        "payment_sequence",
+        "payment_date",
+        "beginning_balance",
+        "principal_payment",
+        "ending_balance",
+        "interest_payment",
+        "fee_payment",
+        "notes",
+    }
+    assert f.shape[0] == 3 * 12
+    f["payment"] = f["interest_payment"] + f["principal_payment"]
+    assert (abs(f["payment"] - 29.96) <= 0.015).all()
